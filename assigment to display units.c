9d513ecb3f13adc21a626aa2962fecec83f8ad21
dopegraham <wafulagraham1@gmail.com>
2025-09-24 08:58:35 +0300

    Add files via upload
    Assigment to display all units of this semester

---
+++ assigment to display units.c
@@ -0,0 +1,19 @@
+//Assignment
+/*
+Name:Graham wafula
+Reg no:PA106/G/28759/25
+Description:program to diplay all units for this semester
+*/
+#include <stdio.h>
+
+int main(){
+	printf("decrete mathematics\n");
+	printf("mathematics for science\n");
+	printf("computer systems and organisatoin\n");
+	printf("fundamentals of ICT\n");
+	printf("introduction to programming and problem solving\n");
+	printf("communication and literacy skills\n");
+	printf("system analysis and organisations\n");
+	
+	return 0;
+}
